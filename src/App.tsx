58e6ff3ee5fd37d529fe89822abc844f905a87b2
Andrey <andrew.gorba4ev2018@yandex.ru>
2021-07-04 12:12:16 +0300

    added project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import UserList from "./components/UserList";
+import TodoList from "./components/TodoList";
+
+const App = () => {
+  return (
+    <div>
+      <UserList />
+      <hr />
+      <TodoList />
+    </div>
+  )
+}
+
+export default App;
